ec7882cfab3bdb19b9e52d27e6e9b805fc177b09
ricardoalexandredev <ricardo.alexandre@frotcom.com>
2020-04-26 15:16:40 +0100

    Add GradeBookType Support to StartingUserInterface

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,12 +34,26 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
             var name = parts[1];
+
+            if (parts[2] == "standard") 
+            {
+                var gradebook = new StandardGradeBook(name);
+            }
+            if (parts[2] == "ranked")
+            {
+                var gradebook = new RankedGradeBook(name);
+            }
+            if (parts[2] != "standard" || parts[2] != "ranked")
+            {
+                Console.WriteLine($"{parts[2]} is not a supported type of gradebook, please try again.");
+            }
+
             BaseGradeBook gradeBook = new BaseGradeBook(name);
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
@@ -67,7 +81,7 @@ namespace GradeBook.UserInterfaces
             Console.WriteLine();
             Console.WriteLine("GradeBook accepts the following commands:");
             Console.WriteLine();
-            Console.WriteLine("Create 'Name' - Creates a new gradebook where 'Name' is the name of the gradebook.");
+            Console.WriteLine("Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.");
             Console.WriteLine();
             Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
             Console.WriteLine();
